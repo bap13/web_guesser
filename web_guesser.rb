ecb53243d40cc8595417128b0b7c8e7a966a6273
Brett Peters <brett.peters1@gmail.com>
2016-01-11 20:23:33 -0700

    finish extensions

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,29 +1,59 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-rand_num = rand(100)
-colors = {:very_wrong => "#C33",
-          :wrong => "#D77",
-          :correct => "#4C4"}
+class Secret
+  attr_accessor :number, :guesses
+  def initialize
+    setup
+  end
+
+  def setup
+    @number = rand(100)
+    @guesses = 5
+  end
+
+  def reset
+    setup
+  end
+end
+
+secret = Secret.new
+colors = {:very_wrong => "#F55",
+          :wrong => "#F88",
+          :correct => "#7F7",
+          :reset => "#77F"}
 
 get '/' do
   message = ""
   guess = params["guess"].to_i
-  if guess > rand_num + 5
-    message = "Way too high!"
-    color = colors[:very_wrong]
-  elsif guess < rand_num - 5
-    message = "Way too low!"
-    color = colors[:very_wrong]
-  elsif guess > rand_num
-    message = "Too high!"
-    color = colors[:wrong]
-  elsif guess < rand_num
-    message = "Too low!"
-    color = colors[:wrong]
+  secret.guesses -= 1
+  if secret.guesses <= 0 && secret.number != guess
+    message = "You ran out of guesses<br><br>New number generated<br><br>"
+    secret.reset
+    color = colors[:reset]
   else
-    message = "You got it right!<br>The SECRET NUMBER is #{rand_num}."
-    color = colors[:correct]
+    if guess > secret.number + 5
+      message = "Way too high!"
+      color = colors[:very_wrong]
+    elsif guess < secret.number - 5
+      message = "Way too low!"
+      color = colors[:very_wrong]
+    elsif guess > secret.number
+      message = "Too high!"
+      color = colors[:wrong]
+    elsif guess < secret.number
+      message = "Too low!"
+      color = colors[:wrong]
+    else
+      message = "You got it right! New number generated."
+      color = colors[:correct]
+      params["cheat"] = "true"
+      secret.reset
+    end
   end
-  erb :index, :locals => {:message => message, :color => color}
+  erb :index, :locals => {:secret => secret.number,
+                          :message => message,
+                          :color => color,
+                          :cheat => params["cheat"],
+                          :guesses => secret.guesses}
 end
